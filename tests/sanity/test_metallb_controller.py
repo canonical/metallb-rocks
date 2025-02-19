8d79124878d7a6318b7cafe2768753f7af75aad3
Etienne Audet-Cobello <etienne.audet-cobello@canonical.com>
2025-02-19 15:58:28 -0500

    adapt metallb_controller test to multiple versions

--- tests/sanity/test_metallb_controller.py
+++ tests/sanity/test_metallb_controller.py
@@ -3,6 +3,9 @@
 # See LICENSE file for licensing details
 #
 
+from typing import List
+
+import pytest
 from k8s_test_harness.util import docker_util, env_util
 
 # In the future, we may also test ARM
@@ -15,16 +18,24 @@ V0_14_5_EXPECTED_FILES = [
 ]
 
 # Just a line that the help string is expected to contain.
-V0_14_5_EXPECTED_HELPSTR = "Usage of /controller:"
+EXPECTED_HELPSTR = "Usage of /controller:"
 
 
-def test_sanity():
+@pytest.mark.parametrize(
+    "metallb_version,expected_files",
+    [
+        ("v0.14.5", V0_14_5_EXPECTED_FILES),
+        ("v0.14.8", V0_14_5_EXPECTED_FILES),
+        ("v0.14.9", V0_14_5_EXPECTED_FILES),
+    ],
+)
+def test_sanity(metallb_version: str, expected_files: List[str]):
     rock = env_util.get_build_meta_info_for_rock_version(
-        IMG_NAME, "v0.14.5", IMG_PLATFORM
+        IMG_NAME, metallb_version, IMG_PLATFORM
     )
 
     docker_run = docker_util.run_in_docker(rock.image, ["/controller", "--help"])
-    assert V0_14_5_EXPECTED_HELPSTR in docker_run.stderr
+    assert EXPECTED_HELPSTR in docker_run.stderr
 
     # check rock filesystem
-    docker_util.ensure_image_contains_paths_bare(rock.image, V0_14_5_EXPECTED_FILES)
+    docker_util.ensure_image_contains_paths_bare(rock.image, expected_files)
