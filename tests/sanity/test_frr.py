7a1b1ba88b2f1b7fc22df57b3d330a94c0b0f80b
Benjamin Schimke <benjamin.schimke@canonical.com>
2025-11-08 07:22:27 +0100

    test: Cleanup and extend test suite
    * Move some of the test logic to the shared k8s-test-harness
    * Add test for FIPS build
    * Add test for pebble version

--- tests/sanity/test_frr.py
+++ tests/sanity/test_frr.py
@@ -3,12 +3,20 @@
 # See LICENSE file for licensing details
 #
 
-import pytest
-from k8s_test_harness.util import docker_util, env_util
+import shlex
+from pathlib import Path
+from typing import List
 
-# In the future, we may also test ARM
-IMG_PLATFORM = "amd64"
-IMG_NAME = "frr"
+import pytest
+from k8s_test_harness.util import docker_util, env_util, fips_util
+
+TEST_PATH = Path(__file__)
+REPO_PATH = TEST_PATH.parent.parent.parent
+IMAGE_NAME = "frr"
+IMAGE_BASE = f"ghcr.io/canonical/{IMAGE_NAME}"
+IMAGE_ENTRYPOINT = "/usr/lib/frr/watchfrr --help"
+PEBBLE_VERSION = "v1.18.0"
+
 
 EXPECTED_FILES = [
     "/usr/sbin/docker-start",
@@ -19,16 +27,62 @@ EXPECTED_FILES = [
 EXPECTED_HELPSTR = "Watchdog program to monitor status of frr daemons"
 
 
-@pytest.mark.parametrize("frr_version", ["9.0.2", "9.1.0", "9.1.3"])
-def test_sanity(frr_version: str):
-    rock = env_util.get_build_meta_info_for_rock_version(
-        IMG_NAME, frr_version, IMG_PLATFORM
+@pytest.mark.parametrize(
+    "version,expected_files",
+    [
+        ("9.0.2", EXPECTED_FILES),
+        ("9.1.0", EXPECTED_FILES),
+        ("9.1.3", EXPECTED_FILES),
+    ],
+)
+def test_filesystem(version: str, expected_files: List[str]):
+    image = env_util.resolve_image(IMAGE_NAME, version)
+    docker_util.ensure_image_contains_paths_bare(image, expected_files)
+
+
+@pytest.mark.parametrize(
+    "image_version", env_util.image_versions_in_repo(IMAGE_NAME, REPO_PATH)
+)
+def test_executable_frr(image_version):
+    image = env_util.resolve_image(IMAGE_NAME, image_version)
+    docker_util.run_entrypoint_and_assert(
+        image, IMAGE_ENTRYPOINT, expect_stdout_contains=EXPECTED_HELPSTR
     )
 
-    # check rock filesystem
-    docker_util.ensure_image_contains_paths(rock.image, EXPECTED_FILES)
 
-    docker_run = docker_util.run_in_docker(
-        rock.image, ["/usr/lib/frr/watchfrr", "--help"]
+@pytest.mark.parametrize(
+    "image_version", env_util.image_versions_in_repo(IMAGE_NAME, REPO_PATH)
+)
+def test_pebble_executable(image_version):
+    image = env_util.resolve_image(IMAGE_NAME, image_version)
+    docker_util.run_entrypoint_and_assert(
+        image, "/bin/pebble version", expect_stdout_contains=PEBBLE_VERSION
     )
-    assert EXPECTED_HELPSTR in docker_run.stdout
+
+
+@pytest.mark.parametrize("GOFIPS", [0, 1], ids=lambda v: f"GOFIPS={v}")
+@pytest.mark.parametrize(
+    "image_version", env_util.image_versions_in_repo(IMAGE_NAME, REPO_PATH)
+)
+def test_fips(image_version, GOFIPS):
+    image = env_util.resolve_image(IMAGE_NAME, image_version)
+    entrypoint = shlex.split(IMAGE_ENTRYPOINT)
+
+    docker_env = ["-e", f"GOFIPS={GOFIPS}"]
+    process = docker_util.run_in_docker(
+        image, entrypoint, check_exit_code=False, docker_args=docker_env
+    )
+
+    rockcraft_yaml = (
+        (REPO_PATH / "frr" / image_version / "rockcraft.yaml").read_text().lower()
+    )
+    expected_returncode, expected_error = fips_util.fips_expectations(
+        rockcraft_yaml, GOFIPS
+    )
+
+    assert (
+        process.returncode == expected_returncode
+    ), f"Return code mismatch for {entrypoint} in image {image}, stderr: {process.stderr}"
+    assert (
+        expected_error in process.stderr
+    ), f"Error message mismatch for {entrypoint} in image {image}, stderr: {process.stderr}"
