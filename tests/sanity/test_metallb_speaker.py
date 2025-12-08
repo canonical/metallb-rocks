dc87b81fdff4de101354e9460db13ee73fb86267
Benjamin Schimke <benjamin.schimke@canonical.com>
2025-12-08 14:36:14 +0100

    feat: add MetalLB v0.15.3 and FRR 10.4.1 support

--- tests/sanity/test_metallb_speaker.py
+++ tests/sanity/test_metallb_speaker.py
@@ -29,6 +29,7 @@ EXPECTED_HELPSTR = "Usage of /speaker:"
         ("v0.14.5", V0_14_5_EXPECTED_FILES),
         ("v0.14.8", V0_14_8_EXPECTED_FILES),
         ("v0.14.9", V0_14_8_EXPECTED_FILES),
+        ("v0.15.3", V0_14_8_EXPECTED_FILES),
     ],
 )
 def test_sanity(metallb_version: str, expected_files: List[str]):
